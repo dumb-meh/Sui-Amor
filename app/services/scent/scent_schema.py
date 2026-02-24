72173f60ca0dbd6acfbff2fb7c33979b30a5a218
dumb-meh <omorfaruk549@gmail.com>
2026-02-24 14:51:35 +0600

    new feture added

--- app/services/scent/scent_schema.py
+++ app/services/scent/scent_schema.py
@@ -8,6 +8,11 @@ class HistoryItem(BaseModel):
 class SubQuestion(BaseModel):
     sub_question: str
     sub_answers: List[str]
+    
+class ScentItem(BaseModel):
+    goal: str
+    value: List[str] | str
+
 
 class QuizItem(BaseModel):
     question: str
@@ -15,21 +20,11 @@ class QuizItem(BaseModel):
     sub_questions: Optional[List[SubQuestion]] = None 
     
 class affirmation_request(BaseModel):
-    existing_profile_tags: Optional[List[str]] = None
     quizdata: List[QuizItem]
-    synergies: Optional[Dict[str, Any]]
-    harmonies: Optional[Dict[str, Any]]
-    resonances: Optional[Dict[str, Any]]
-    polarities: Optional[Dict[str, Any]]
-    past_theme: Optional[List[str]] = None
-    past_affirmations: Optional[List[List[str]]] = None
-    religious_or_spritual_preference: Optional[str] = None
-    holiday_preference: Optional[str] = None
-    astrology_preference: Optional[str] = None
-    affirmation_type: str
+    base_scent_info:List[ScentItem]
+
     
 class affirmation_response(BaseModel):
     affirmation: List[str]
     affirmation_theme:str
-    short_summary_of_quiz: str
 
