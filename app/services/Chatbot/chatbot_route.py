5105729ea440e6936aad4000b16f7c8e528965f9
dumb-meh <omorfaruk549@gmail.com>
2026-06-09 10:48:34 +0600

    fixed redis in docker compose file

--- app/services/Chatbot/chatbot_route.py
+++ app/services/Chatbot/chatbot_route.py
@@ -2,23 +2,22 @@ from fastapi import APIRouter, HTTPException, Header
 from .chatbot_schema import ChatbotMessageRequest, ChatbotMessageResponse
 from .chatbot import ChatbotService
 
-router = APIRouter()
+
+router = APIRouter(tags=["Chatbot"])
 chatbot_service = ChatbotService()
 
 
 
 
-
-@router.post("/", response_model=ChatbotMessageResponse)
-async def chatbot(
+@router.post("/", response_model=ChatbotMessageResponse, summary="Send a message to the chatbot")
+async def chatbot_web_message(
     request: ChatbotMessageRequest,
-
 ):
-
     user_id = request.user_id
     try:
         response = chatbot_service.get_response(user_id, request.user_message, "web")
         return ChatbotMessageResponse(chatbot_reply=response)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
